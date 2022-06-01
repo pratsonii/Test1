542b276783f43e0946d517f3593ee9246f53a32f
Pratik Soni <100229748+PratikLaweb@users.noreply.github.com>
2022-06-01 22:42:15 +0530

    First Commit

---
+++ src/main/java/com/example/demo/RequestScopeBean1testApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestScopeBean1testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestScopeBean1testApplication.class, args);
+	}
+
+}
